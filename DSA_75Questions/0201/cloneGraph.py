9bbe2222abae370b9f9eee3af3ab7e33c76e0322
Rahul Patil <rahulpatil.stallion@gmail.com>
2022-01-02 13:33:23 +0530

    Add files via upload

---
+++ DSA_75Questions/0201/cloneGraph.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def cloneGraph(self, node):
+        
+        oldToNew = {}
+        
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        
+        return dfs(node) if node else None
