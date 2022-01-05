5be2b51051c23e2f40910db7d1e36954c9cd10b2
Rahul Patil <rahulpatil.stallion@gmail.com>
2022-01-05 11:50:59 +0530

    Add files via upload

---
+++ DSA_75Questions/0501/isSubtree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+        if not subRoot: return True
+        if not root: return False
+        
+        if self.sameTree(root, subRoot):
+            return True
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+        
+    
+    def sameTree(self, s, t):
+        if not s and not t:
+            return True
+        if s and t and s.val == t.val:
+            return (self.sameTree(s.left ,t.left) and self.sameTree(s.right, t.right))
+        return False        
