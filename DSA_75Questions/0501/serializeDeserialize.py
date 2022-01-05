7a04b9f9880e22fc760d2968bf1306f73e63ea77
Rahul Patil <rahulpatil.stallion@gmail.com>
2022-01-05 11:24:09 +0530

    Add files via upload

---
+++ DSA_75Questions/0501/serializeDeserialize.py
@@ -0,0 +1,44 @@
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        res = []
+        
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(res)
+    
+            
+            
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals = data.split(",")
+        self.i = 0
+        
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left =dfs()
+            node.right = dfs()
+            return node
+        return dfs()
+       
