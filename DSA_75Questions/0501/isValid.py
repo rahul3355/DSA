0edd7565685c29dff3b63d4b26a395c4045836f6
Rahul Patil <rahulpatil.stallion@gmail.com>
2022-01-06 11:13:18 +0530

    Add files via upload

---
+++ DSA_75Questions/0501/isValid.py
@@ -0,0 +1,8 @@
+def valid(node, left, right):
+            if not node:
+                return True
+            if not (node.val < right and node.val > left):
+                return False
+            
+            return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
+        return valid(root, float("-inf"), float("inf"))
