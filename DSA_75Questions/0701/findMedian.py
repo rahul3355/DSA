7711504b2317adcb5534888ce28b92a9e87d5543
Rahul Patil <rahulpatil.stallion@gmail.com>
2022-01-07 12:47:54 +0530

    Add files via upload

---
+++ DSA_75Questions/0701/findMedian.py
@@ -0,0 +1,27 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small, self.large = [],[]        
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -1 * num)
+        
+        if (self.small and self.large and (-1 * self.small[0]) > self.large[0]):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+            
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+        
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        
+        return (-1 * self.small[0] + self.large[0]) / 2
+        
