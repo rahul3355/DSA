0ba8cd1115ad8805dd82c486a28968f743b65fbb
Rahul Patil <rahulpatil.stallion@gmail.com>
2022-02-02 13:24:18 +0530

    Add files via upload

---
+++ DSA_75Questions/0202/dedcodeWays.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        dp = { len(s) : 1}
+        
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i] == "0":
+                return 0
+            
+            res = dfs(i+1)
+            if (i+1 < len(s) and (s[i] == "1" or s[i]=="2" and
+                                 s[i+1] in "0123456")):
+                res += dfs(i+2)
+            dp[i] = res
+            return res
+        return dfs(0)
