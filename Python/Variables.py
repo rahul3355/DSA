b5f82ac82d44ac118f78746cbeea832079fb0647
unknown <rahul>
2023-07-20 06:47:49 +0530

    Python for tech interviews

---
+++ Python/Variables.py
@@ -0,0 +1,16 @@
+# Variables are dynamically typed
+
+n = 0
+print("n = ", n)
+
+n = "abc"
+print("n = ", n)
+
+n, m = 0, "abc"
+print("n = ", n " m = ", m)
+
+n += 1
+print("n = ", n)
+
+n = None
+print("n = ", n)
