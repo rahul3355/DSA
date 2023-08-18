1614a06c958bfb7c11333b4f6779131bea3d21da
unknown <rahul>
2023-08-18 15:50:09 +0530

    productExceptSelf

---
+++ Blind75/groupAnagrams.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        res = defaultdict(list) # mapping charCount list to the string
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)
+            
+        return res.values()
+        
+        # O (m * n)
---
+++ Blind75/topKFrequent.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+        # O(n) time | O(n) space
