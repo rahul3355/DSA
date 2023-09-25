73ef2119daa5b0ce73de6ad7c87a93aa4ee41822
Rahul Vijay Patil <rahulvijay.patil@postgrad.manchester.ac.uk>
2023-09-25 16:57:38 +0100

    b3

---
+++ practice/p1/b3.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        
+        return
